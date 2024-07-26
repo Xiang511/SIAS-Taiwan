03902776c24c4b51c1dd7b3d187b5b7ca74e3a2b
Xiang511 <toby91511@gmail.com>
2024-07-26 23:29:38 +0800

    feat: Change background color when nav is scrolled

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ for (var i = 0; i < menuBtn.length; i++) {
         e.target.classList.toggle("active");
     });
 }
+
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('.main-header');
+    if (window.scrollY > 50) { // 當捲動超過50px時改變背景顏色
+        nav.classList.add('nav-scrolled');
+        nav.classList.remove('nav-default');
+    } else {
+        nav.classList.add('nav-default');
+        nav.classList.remove('nav-scrolled');
+    }
+});
